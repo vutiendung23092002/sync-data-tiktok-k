f8daa130a838da813c419465a160f675d1849c6e
vutiendung23092002 <vutiendung23092002@gmail.com>
2025-12-02 14:10:25 +0700

    đổi choice re auth qua string

--- re-auth-token-tiktok.js
+++ re-auth-token-tiktok.js
@@ -2,7 +2,7 @@ import { reAuthTokenTiktok } from "./src/services/tiktok/re-auth-tiktok.js";
 import { env } from "./src/config/env.js";
 
 async function reAuthTiktok(authCode, choice) {
-  if (choice == 1) {
+  if (choice == "1") {
     console.log("Re-auth cho app K");
     await reAuthTokenTiktok(
       authCode,
@@ -12,7 +12,7 @@ async function reAuthTiktok(authCode, choice) {
       env.TIKTOK.shop_k_lady_care_7527154834987157254.app_name,
       env.TIKTOK.shop_k_lady_care_7527154834987157254.web
     );
-  } else if (choice == 2) {
+  } else if (choice == "2") {
     console.log("Re-auth cho app Han");
     await reAuthTokenTiktok(
       authCode,
